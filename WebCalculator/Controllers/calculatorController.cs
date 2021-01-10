dcc5ea40e81ff6543f8dcfe6ae1bcffd0959ecab
MRPhillip007 <42628822+MRPhillip007@users.noreply.github.com>
2021-01-10 14:39:10 +0300

    first commit

---
+++ WebCalculator/Controllers/calculatorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using WebCalculator.Models;
+
+namespace WebCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: calculator
+        public ActionResult Index()
+        {
+            return View(new Calculator());  
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calculator calc, string calculate)
+        {
+            if (calculate == "plus")
+            {
+                calc.Result = calc.Number1 + calc.Number2;
+            }
+
+            else if (calculate == "minus")
+            {
+                calc.Result = calc.Number1 - calc.Number2;
+            }
+
+            else if (calculate == "multiplication")
+            {
+                calc.Result = calc.Number1 * calc.Number2;
+            }
+            else
+            {
+                calc.Result = calc.Number1 / calc.Number2;
+            }
+
+            return View(calc);
+        }
+    }
+}
